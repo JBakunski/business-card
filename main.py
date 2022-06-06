a8eff4fab96b712d69001ffce87126ffdafaa68c
Jacek Bakuński <jacek.bakunski@gmail.com>
2022-06-06 18:57:50 +0200

    Implement new classes

--- main.py
+++ main.py
@@ -1,27 +1,31 @@
-
 from faker import Faker
 
-class BusinessCard:
-    def __init__(self, first_name, last_name, occupation, email):
+class BaseContact:
+    def __init__(self, first_name, last_name, phone_number, email):
         self.first_name = first_name
         self.last_name = last_name
-        self.occupation = occupation
+        self.phone_number = phone_number
         self.email = email
 
-        self._fullname_length = ''
-
-
-    def display_info(self):
-        print(f"Imię: {self.first_name}, Nazwisko: {self.last_name}, Stanowisko: {self.occupation}, Adres email: {self.email}")
-    
-    def contact(self):
-        print(f"Kontaktuje się z {self.first_name}, {self.last_name}, {self.occupation}, {self.email}")
+        self._lable_length = ''
     
     @property
-    def fullname_length(self):
+    def label_length(self):
         return f"{len(self.first_name)}_{len(self.last_name)}"
-
     
+    def contact(self):
+        print(f"Wybieram number {self.phone_number} i dzwonię do {self.first_name} {self.last_name}.")
+
+
+class BusinessContact(BaseContact):
+    def __init__(self, occupation, company_name, business_phone, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.occupation = occupation
+        self.company_name = company_name
+        self.business_phone = business_phone
+    
+    def contact(self):
+        print(f"Wybieram number {self.business_phone} i dzwonię do {self.first_name} {self.last_name}.")
 
 fake = Faker()
 
